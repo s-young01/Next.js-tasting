e6f67cb52885533e0c28b3b40fe60af3be60056b
s-young01 <119985133+s-young01@users.noreply.github.com>
2023-05-03 16:31:56 +0900

    next.js

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,20 +1,21 @@
-import Hi from './data.js';
 
 export default function Cart() {
+    let 장바구니 = ['Tomatoes', 'Pasta'];
     return (
         <div>
-            {Hi}
             <h2 className="title">장바구니</h2>
-            <div className="cart-item">
-                <p>상품명</p>
-                <p>$40</p>
-                <p>1개</p>
-            </div>
-            <div className="cart-item">
-                <p>상품명</p>
-                <p>$40</p>
-                <p>1개</p>
-            </div>
+            <CartItem item={장바구니[0]}/>
+            <CartItem item={장바구니[1]}/>
+        </div>
+    )
+  }
+
+  function CartItem(props) {
+    return (
+        <div className="cart-item">
+            <p>{props.item}</p>
+            <p>$40</p>
+            <p>1개</p>
         </div>
     )
   }
--- app/list/page.js
+++ app/list/page.js
@@ -1,16 +1,42 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react";
 
 export default function List() {
     let 상품 = ['Tomatoes','Pasta','Coconut'];
+    // 상품수량 상태관리
+    let [수량, 수량변경] = useState([1, 1, 1]);
     return (
       <div>
         <h2 className="title">상품목록</h2>
         {상품.map((a, i) => {
             return (
                 <div className="food" key={i}>
-                    <Image src={`/food${i}.png`} 
-                    width={500} height={300} className="food-img"/>
+                    <img src={`/food${i}.png`} className="food-img"/>
                     <h4>{a} $40</h4>
+                    <nav>
+                      <span>상품수량</span>
+                      <span> : </span>
+                      <span>{수량[i]} 개 </span>
+                      <button onClick={() => {
+                        let count = [...수량]
+                        if(count[i] >= 10) {
+                          alert('최대 주문 수량은 10개입니다.')
+                        }else {
+                          count[i]++
+                          수량변경(count)
+                        }
+                        }}>+</button>
+                      <button onClick={() => {
+                        let count = [...수량]
+                        if(count[i] <= 1) {
+                          alert('최소 주문 수량은 1개입니다.')
+                        }else {
+                          count[i]--
+                          수량변경(count)
+                        }
+                        }}>-</button>
+                    </nav>
                 </div>
             )
         })}
